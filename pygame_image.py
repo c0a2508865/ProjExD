b4daa94f45ab3d106836111037c108dd774ab4db
koharu <c0a2508865@edu.teu.ac.jp>
2026-06-09 17:05:03 +0900

    演習課題２：move_ipの使用を一回だけにした

--- pygame_image.py
+++ pygame_image.py
@@ -22,15 +22,17 @@ def main():
 
         key_lst = pg.key.get_pressed()  # 練習10-3：すべてのキーの押下状態の取得        
         # print(key_lst)
-        kk_rct.move_ip(-1, 0)
-        if key_lst[pg.K_UP]:  # 上矢印キーが押されたら
-            kk_rct.move_ip(0, -1)  # 上に移動
+        vx, vy = -1, 0
+        if key_lst[pg.K_UP]:    # 上矢印キーが押されたら
+            vy -= 1             # Y方向の移動量を「上の負の方向」へ増やす
         if key_lst[pg.K_DOWN]:  # 下矢印キーが押されたら
-            kk_rct.move_ip(0, +1)  # 下に移動
-        if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip(-1, 0)
-        if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip(+1, 0)
+            vy += 1             # Y方向の移動量を「下の正の方向」へ増やす
+        if key_lst[pg.K_LEFT]:  # 左矢印キーが押されたら
+            vx -= 1             # X方向の移動量を「さらに左」へ増やす
+        if key_lst[pg.K_RIGHT]: # 右矢印キーが押されたら
+            vx += 1
+
+        kk_rct.move_ip(vx, vy)
 
         x = tmr%3200
         screen.blit(bg_img, [-x, 0])  # 練習5：背景画像を右から左へ
